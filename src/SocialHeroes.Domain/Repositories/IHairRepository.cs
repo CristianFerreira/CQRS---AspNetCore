118d0e4eb73662ed2acd0947ab13741ae137c2ea
Cristian Ferreira <cristianferreira_gks@hotmail.com>
2018-10-19 00:10:33 -0300

    pull demo project CQRS

---
+++ src/SocialHeroes.Domain/Repositories/IHairRepository.cs
@@ -0,0 +1,12 @@
+﻿using SocialHeroes.Domain.DonatorContext.Models;
+using SocialHeroes.Domain.DonatorContext.Queries;
+using System.Collections.Generic;
+
+namespace SocialHeroes.Domain.DonatorContext.Repositories
+{
+    public interface IHairRepository
+    {
+        void Save(Hair hair);
+        IEnumerable<ListHairQueryResult> Get();
+    }
+}
---
+++ src/SocialHeroes.Shared/Commands/ICommandResult.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialHeroes.Shared.Commands
+{
+    public interface ICommandResult
+    {
+        bool Sucess { get; set; }
+        string Message { get; set; }
+        object Data { get; set; }
+    }
+}
